6e478057f9434a8af2155d903f22664caf32e854
Bea-Xavier <bvinguerti@gmail.com>
2026-04-06 22:34:44 -0300

    feat: Exemplificações

--- frontend/meuCrudApp/src/servers/configApi.js
+++ frontend/meuCrudApp/src/servers/configApi.js
@@ -1,5 +1,5 @@
 // URL gerada pelo localTunnel, para acessar a API do JSON Server, é alterada a cada execução
-export const API_URL = 'https://lovely-jars-grin.loca.lt';
+export const API_URL = 'https://tidy-buckets-visit.loca.lt';
 
 //URL para testar localmente, somente para desenvolvimento
 // export const API_URL = 'http://localhost:3000';
